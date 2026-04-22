b8963060653b5f097ad466b3976ba1003a687fa2
constant <biolivesztrleskovac@gmail.com>
2026-04-22 11:25:19 +0200

    novo

--- app.py
+++ app.py
@@ -1,55 +1,56 @@
-import os
-import sqlite3
-from flask import Flask, request, jsonify, json, render_template
-from flask_cors import CORS
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route("/")
 def index():
     nazivSpiska = "Spisak restorana"
-    spisakRestorana = ["Pastica","Pica Tim","HasHub","Sahara"]
+    spisakRestorana = ["Pastica", "Pica Tim", "HasHub", "Sahara", "ABC", "Lele", "Oskar", "Cap Cap", "Promenada"]
     return render_template("index.html", naziv=nazivSpiska, spisak=spisakRestorana)
 
 @app.route("/restorani")
 def restorani():
-    nazivRestorana = "Spisak restorana"
-    spisakRestorani = ["Pastica","Pica Tim","HasHub","Sahara"]
+    nazivRestorana = "Svi restorani"
+    spisakRestorani = ["Pastica", "Pica Tim", "HasHub", "Sahara", "ABC", "Lele", "Oskar", "Cap Cap", "Promenada"]
     return render_template("restorani.html", naziv=nazivRestorana, spisak=spisakRestorani)
 
 @app.route("/restorani/1")
 def meni():
-    nazivMeni = "Meni restorana"
-    spisakMeni = ["Pastica","Pica Tim","HasHub","Sahara"]
-    return render_template("meno.html", naziv=nazivMeni, spisak=spisakMeni)
+    nazivMeni = "Meni restorana Pica Tim"
+    spisakMeni = ["Pica Margarita", "Pica Capricciosa", "Pica Vesuvio", "Pica Quattro Stagioni", "Pica Napolitana"]
+    return render_template("meni.html", naziv=nazivMeni, spisak=spisakMeni)
 
 @app.route("/primer-niz")
 def niz():
-    nekiNiz = [1,2,3,4,5]
+    nekiNiz = [1, 2, 3, 4, 5]
     return jsonify(nekiNiz)
 
 @app.route("/primer-json")
 def primerJson():
-    data = {"message":"This is a JSON response","status":"success"}
+    data = {"message": "This is a JSON response", "status": "success"}
     return jsonify(data)
 
 @app.route("/primer-html")
 def primerHTML():
-    data = """<!DOCTYPE html>
-    <html lang="en">
+    data = """
+    <!DOCTYPE html>
+    <html lang="sr">
     <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="/static/style.css">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Primer HTML</title>
+        <link rel="stylesheet" href="/static/style.css">
     </head>
     <body>
-        <h1>Zdravo programeri</h1>
+        <div class="container sredina">
+            <h1>Zdravo programeri</h1>
+            <p>Ovo je primer HTML stranice</p>
+            <a href="/" class="btn">Nazad na početnu</a>
+        </div>
     </body>
-    </html>"""
+    </html>
+    """
     return data
 
-if __name__== "__main__":
+if __name__ == "__main__":
     app.run(debug=True)
