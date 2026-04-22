e05bed221a40d169590f4f6244123e45cb2bdd76
constant <biolivesztrleskovac@gmail.com>
2026-04-22 11:14:52 +0200

    dad

---
+++ app copy.py
@@ -0,0 +1,54 @@
+import os
+import sqlite3
+from flask import Flask, request, jsonify, json, render_template
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def index():
+    nazivSpiska = "Spisak restorana"
+    spisakRestorana = ["Pastica", "Pica Tim", "HasHub", "Sahara"]
+    return render_template("index.html", naziv=nazivSpiska, spisak=spisakRestorana)
+
+@app.route("/restorani")
+def restorani():
+    nazivRestorana = "Spisak restorana"
+    spisakRestorani = ["Pastica", "Pica Tim", "HasHub", "Sahara"]
+    return render_template("restorani.html", naziv=nazivRestorana, spisak=spisakRestorani)
+
+@app.route("/restorani/1")
+def meni():
+    nazivMeni = "Meni restorana"
+    spisakMeni = ["Pica Margarita", "Pica Capricciosa", "Pica Vesuvio", "Pica Quattro Stagioni"]
+    return render_template("meni.html", naziv=nazivMeni, spisak=spisakMeni)
+
+@app.route("/primer-niz")
+def niz():
+    nekiNiz = [1, 2, 3, 4, 5]
+    return jsonify(nekiNiz)
+
+@app.route("/primer-json")
+def primerJson():
+    data = {"message": "This is a JSON response", "status": "success"}
+    return jsonify(data)
+
+@app.route("/primer-html")
+def primerHTML():
+    data = """<!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/static/css/style.css">
+    </head>
+    <body>
+        <h1>Zdravo programeri</h1>
+    </body>
+    </html>"""
+    return data
+
+if __name__ == "__main__":
+    app.run(debug=True)
